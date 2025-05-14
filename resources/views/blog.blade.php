a1ac1eb3858228f0b7cac748847ce3ba95e4a977
ycnugroho” <5026231032@student.its.ac.id>
2025-05-14 09:21:14 +0700

    pertemuan 11

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial Laravel - www.malasngoding.com</title>
+</head>
+<body>
+
+	<h3>www.malasngoding.com</h3>
+	<p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
+	<p>Ini adalah view blog. ada di route blog.</p>
+
+</body>
+</html>
+
+<?php
+
+echo date('l, d-m-Y');
+
+echo "<br/>";
+
+echo date('d / M / y');
+
+echo "<br/>";
+
+echo date('D - M / Y');
+
+?>
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Coba;
+
+//function
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/selamat', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+	return "<h1>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h1>";
+});
+
+Route::get('blog', function () {
+	return view('blog');
+});
+
+Route::get('hello',[Coba::class,'helloworld']);
